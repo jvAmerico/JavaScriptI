47ddf8af88a46bf265e8574c30e34c3e41d18da0
Joao-Americo <167218220+jvAmerico@users.noreply.github.com>
2024-04-20 12:42:17 -0300

    definicao do cauculo automatico da idade

--- PrimeiroArquivo/Script.js
+++ PrimeiroArquivo/Script.js
@@ -1,9 +1,14 @@
-anoNascimento = 2000
+//input
+anoNascimento = 1984
+nome = "Joao Victor"
 
-idade = 2024 - anoNascimento
+//processamento
+hoje = new Date();
+anoAtual = hoje.getFullYear();
+idade = anoAtual - anoNascimento
+mensagem = "Sou" + nome + "e tenho" + idade+ "anos"
 
-nome = "Renato"
-
-mensagem = "Sou" + nome + "e tenho" + idade + "anos"
-
+//output
 alert(mensagem)
+
+
